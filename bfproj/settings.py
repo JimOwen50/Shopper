f0228f56324593bebdf976c9a991394d1b1e2191
Jim Owen <jim.owen50@gmail.com>
2023-11-19 17:29:04 -0500

    Committing V1.1

--- bfproj/settings.py
+++ bfproj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'giftlist',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- giftlist/forms.py
+++ giftlist/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 from .models import Gift, Member, Profile
